f033da41a3f490b60c32bb2d78b964d7499ec59d
DemetreD <136166143+DemetreD@users.noreply.github.com>
2025-08-14 16:57:40 -0700

    added play game function

--- script.js
+++ script.js
@@ -6,40 +6,44 @@ const getComputerChoice = function () {
   return randomChoice;
 };
 
-// console.log(getComputerChoice());
-
 //human choice
 const getHumanChoice = function (option) {
   return option;
 };
 
-// console.log(getHumanChoice(userInput));
+const playGame = function () {
+  let humanScore = 0;
+  let computerScore = 0;
 
-let humanScore = 0;
-let computerScore = 0;
+  const playRound = function (humanChoice, computerChoice) {
+    if (
+      (humanChoice.toLowerCase() === "rock" && computerChoice === "Scissors") ||
+      (humanChoice.toLowerCase() === "paper" && computerChoice === "Rock") ||
+      (humanChoice.toLowerCase() === "scissors" && computerChoice === "Paper")
+    ) {
+      humanScore++;
+      console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+    }
 
-const playRound = function (humanChoice, computerChoice) {
-  if (
-    (humanChoice.toLowerCase() === "rock" && computerChoice === "Scissors") ||
-    (humanChoice.toLowerCase() === "paper" && computerChoice === "Rock") ||
-    (humanChoice.toLowerCase() === "scissors" && computerChoice === "Paper")
-  ) {
-    humanScore++;
-    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
-  }
+    if (
+      (computerChoice === "Rock" && humanChoice.toLowerCase() === "scissors") ||
+      (computerChoice === "Paper" && humanChoice.toLowerCase() === "rock") ||
+      (computerChoice === "Scissors" && humanChoice.toLowerCase() === "paper")
+    ) {
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+  };
 
-  if (
-    (computerChoice === "Rock" && humanChoice.toLowerCase() === "scissors") ||
-    (computerChoice === "Paper" && humanChoice.toLowerCase() === "rock") ||
-    (computerChoice === "Scissors" && humanChoice.toLowerCase() === "paper")
-  ) {
-    computerScore++;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  //play
+
+  for (let i = 0; i < 5; i++) {
+    const userInput = prompt(`Rock, Paper, Scissors?`);
+
+    const humanSelection = getHumanChoice(userInput);
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
   }
 };
 
-const userInput = prompt(`Rock, Paper, Scissors?`);
-
-const humanSelection = getHumanChoice(userInput);
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+playGame();
