e9d339d29c0acb20160f586f2a2a8cb8d07e5e31
DemetreD <136166143+DemetreD@users.noreply.github.com>
2025-08-13 23:03:11 -0700

    added play round function

--- script.js
+++ script.js
@@ -13,5 +13,33 @@ const getHumanChoice = function (option) {
   return option;
 };
 
+// console.log(getHumanChoice(userInput));
+
+let humanScore = 0;
+let computerScore = 0;
+
+const playRound = function (humanChoice, computerChoice) {
+  if (
+    (humanChoice.toLowerCase() === "rock" && computerChoice === "Scissors") ||
+    (humanChoice.toLowerCase() === "paper" && computerChoice === "Rock") ||
+    (humanChoice.toLowerCase() === "scissors" && computerChoice === "Paper")
+  ) {
+    humanScore++;
+    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+  }
+
+  if (
+    (computerChoice === "Rock" && humanChoice.toLowerCase() === "scissors") ||
+    (computerChoice === "Paper" && humanChoice.toLowerCase() === "rock") ||
+    (computerChoice === "Scissors" && humanChoice.toLowerCase() === "paper")
+  ) {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  }
+};
+
 const userInput = prompt(`Rock, Paper, Scissors?`);
-console.log(getHumanChoice(userInput));
+
+const humanSelection = getHumanChoice(userInput);
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
