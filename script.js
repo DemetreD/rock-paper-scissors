49f4ac049070ec39dbc243d60753b7375460c00a
DemetreD <136166143+DemetreD@users.noreply.github.com>
2025-08-15 20:23:39 -0700

    added game logic

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
 //computer choice
 const getComputerChoice = function () {
-  const options = ["Rock", "Paper", "Scissors"];
+  const options = ["rock", "paper", "scissors"];
   const choice = Math.floor(Math.random() * options.length);
   let randomChoice = options[choice];
   return randomChoice;
 };
 
 //human choice
-const getHumanChoice = function (option) {
-  return option;
+const getHumanChoice = function () {
+  let userInput = prompt("Rock, Paper, Scissors?");
+  return userInput.toLowerCase();
 };
 
+//game
 const playGame = function () {
+  //round function
   let humanScore = 0;
   let computerScore = 0;
 
   const playRound = function (humanChoice, computerChoice) {
-    if (
-      (humanChoice.toLowerCase() === "rock" && computerChoice === "Scissors") ||
-      (humanChoice.toLowerCase() === "paper" && computerChoice === "Rock") ||
-      (humanChoice.toLowerCase() === "scissors" && computerChoice === "Paper")
+    if (humanChoice === computerChoice) {
+      console.log(`It's a Tie!`);
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
     ) {
       humanScore++;
-      console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
-    }
-
-    if (
-      (computerChoice === "Rock" && humanChoice.toLowerCase() === "scissors") ||
-      (computerChoice === "Paper" && humanChoice.toLowerCase() === "rock") ||
-      (computerChoice === "Scissors" && humanChoice.toLowerCase() === "paper")
-    ) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else {
       computerScore++;
       console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
     }
   };
 
-  //play
-
+  //5 rounds game
   for (let i = 0; i < 5; i++) {
-    const userInput = prompt(`Rock, Paper, Scissors?`);
-
-    const humanSelection = getHumanChoice(userInput);
+    const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
   }
